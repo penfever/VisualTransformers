398ec2081222740a45a350dd525a01d20d040e19
penfever <penfever@gmail.com>
2022-06-06 12:07:25 -0400

    cleaned ResViT (no seq learning)

--- ResViT.py
+++ ResViT.py
@@ -8,6 +8,7 @@ from einops import rearrange
 from torch import nn
 import torch.nn.init as init
 import math
+from torch.nn import MultiheadAttention
 
 def _weights_init(m):
     classname = m.__class__.__name__
@@ -15,103 +16,62 @@ def _weights_init(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
         init.kaiming_normal_(m.weight)
 
-class LambdaLayer(nn.Module):
-    def __init__(self, lambd):
-        super(LambdaLayer, self).__init__()
-        self.lambd = lambd
-
-    def forward(self, x):
-        return self.lambd(x)
-
-class Residual(nn.Module):
-    def __init__(self, fn):
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, mlp_dim, dropout):
         super().__init__()
-        self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(x, **kwargs) + x
-
-class LayerNormalize(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-class MLP_Block(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.1):
-        super().__init__()
-        self.nn1 = nn.Linear(dim, hidden_dim)
+        self.dim = dim
+        self.attn = MultiheadAttention(embed_dim=dim, num_heads = heads, dropout = dropout, batch_first=True)
+        self.layer_norm = nn.LayerNorm(dim)
+        self.nn1 = nn.Linear(dim, mlp_dim)
         torch.nn.init.xavier_uniform_(self.nn1.weight)
         torch.nn.init.normal_(self.nn1.bias, std = 1e-6)
         self.af1 = nn.GELU()
         self.do1 = nn.Dropout(dropout)
-        self.nn2 = nn.Linear(hidden_dim, dim)
+        self.nn2 = nn.Linear(mlp_dim, dim)
         torch.nn.init.xavier_uniform_(self.nn2.weight)
         torch.nn.init.normal_(self.nn2.bias, std = 1e-6)
         self.do2 = nn.Dropout(dropout)
-        
-    def forward(self, x):
+
+    def forward(self, x, mask = None):
+        identity = x
+        x = self.attn(x, x, x)
+        x = self.layer_norm(x[0]) + identity
+        identity = x
         x = self.nn1(x)
         x = self.af1(x)
         x = self.do1(x)
         x = self.nn2(x)
         x = self.do2(x)
-        
+        x = self.layer_norm(x) + identity
         return x
 
-class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dropout = 0.1):
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
         super().__init__()
-        self.heads = heads
-        self.scale = dim ** -0.5  # 1/sqrt(dim)
-
-        self.to_qkv = nn.Linear(dim, dim * 3, bias = True) # Wq,Wk,Wv for each vector, thats why *3
-        torch.nn.init.xavier_uniform_(self.to_qkv.weight)
-        torch.nn.init.zeros_(self.to_qkv.bias)
+        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        # max_len determines how far the position can have an effect on a token (window)
         
-        self.nn1 = nn.Linear(dim, dim)
-        torch.nn.init.xavier_uniform_(self.nn1.weight)
-        torch.nn.init.zeros_(self.nn1.bias)        
-        self.do1 = nn.Dropout(dropout)
+        # Info
+        self.dropout = nn.Dropout(dropout_p)
         
-
-    def forward(self, x, mask = None):
-        b, n, _, h = *x.shape, self.heads
-        qkv = self.to_qkv(x) #gets q = Q = Wq matmul x1, k = Wk mm x2, v = Wv mm x3
-        q, k, v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv = 3, h = h) # split into multi head attentions
-
-        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
-
-        if mask is not None:
-            mask = F.pad(mask.flatten(1), (1, 0), value = True)
-            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
-            mask = mask[:, None, :] * mask[:, :, None]
-            dots.masked_fill_(~mask, float('-inf'))
-            del mask
-
-        attn = dots.softmax(dim=-1) #follow the softmax,q,d,v equation in the paper
-
-        out = torch.einsum('bhij,bhjd->bhid', attn, v) #product of v times whatever inside softmax
-        out = rearrange(out, 'b h n d -> b n (h d)') #concat heads into one matrix, ready for next encoder block
-        out =  self.nn1(out)
-        out = self.do1(out)
-        return out
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, mlp_dim, dropout):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                Residual(LayerNormalize(dim, Attention(dim, heads = heads, dropout = dropout))),
-                Residual(LayerNormalize(dim, MLP_Block(dim, mlp_dim, dropout = dropout)))
-            ]))
-    def forward(self, x, mask = None):
-        for attention, mlp in self.layers:
-            x = attention(x, mask = mask) # go to attention
-            x = mlp(x) #go to MLP_Block
-        return x
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+        
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+        
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding",pos_encoding)
+        
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
 
 def build_seq(image_vec, label_vec):
     embed_full = image_vec[0]
@@ -138,25 +98,22 @@ class seqTrans(nn.Module):
         self.n_seq = BATCH_SIZE_TRAIN // self.num_tokens
         self.conv_model = conv_model
         self.in_planes = 64 #controls how many channels the model expects
-        self.label_embed = torch.nn.Embedding(num_classes, dim) * math.sqrt(self.dim)
+        self.label_embed = torch.nn.Embedding(num_classes, dim)
         self.num_classes = num_classes
         self.apply(_weights_init)
-        
-        self.pos_embedding = nn.Parameter(torch.empty(BATCH_SIZE_TRAIN // self.num_tokens, num_tokens * 2, dim))        
-        torch.nn.init.normal_(self.pos_embedding, std = .02) # Initialize to normal distribution. Based on the paper
+        self.positional_encoder = PositionalEncoding(
+            dim_model=dim, dropout_p=dropout, max_len=5000
+        )
         self.dropout = nn.Dropout(emb_dropout)
 
         self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
-        # self.transformer = torch.nn.Transformer(d_model=dim)
-        # decoder_layer = nn.TransformerDecoderLayer(d_model=dim, nhead=heads)
-        # self.transformer = nn.TransformerDecoder(decoder_layer, num_layers=depth)
         self.to_cls_token = nn.Identity()  #TODO: consider using linear, tanh for this a la BERT
         self.nn1 = nn.Linear(dim, self.num_classes)  # if finetuning, just use a linear layer without further hidden layers (paper)
         torch.nn.init.xavier_uniform_(self.nn1.weight)
         torch.nn.init.normal_(self.nn1.bias, std = 1e-6)
         
     def forward(self, seq_x, seq_y, mask = None):
-        x = conv_model(seq_x.view(BATCH_SIZE_TRAIN, 3, 105, 105)) * math.sqrt(self.dim)
+        x = conv_model(seq_x.view(BATCH_SIZE_TRAIN, 3, 105, 105))
         # print(x.size())
         # x = x.()
         # x = rearrange(x, 'b c h w -> b (h w) c') # nXn convolution output reshaped to [batch_size, (n^2), c]
@@ -166,7 +123,7 @@ class seqTrans(nn.Module):
             seq_y = seq_y.reshape(BATCH_SIZE_TRAIN)
             seq_y[idx] = self.num_classes-1 #CLS_TOKEN
             # print(seq_y)
-            y = self.label_embed(seq_y)
+            y = self.label_embed(seq_y) * (self.dim ** -0.5)
             # print("After label embed: ")
             # print(y.size(), x.size())
             # y = y.view(len(seq_y), self.dim, self.in_planes)
@@ -174,28 +131,27 @@ class seqTrans(nn.Module):
             x = x.reshape(self.n_seq, self.num_tokens * 2, self.dim)
         # print("After sequence reshaping: ")
         # print(x.size(), self.pos_embedding.size())
-        x += self.pos_embedding
+        x = self.positional_encoder(x)
         x = self.dropout(x)
         # mask = torch.ones_like(torch.tensor([len(x),len(x)])).bool()
-        # tgt = torch.rand_like(x)
-        # tgt = self.transformer(tgt, x)
-        x = self.transformer(x, None)
+        tgt = torch.rand_like(x)
+        tgt = self.transformer(tgt, x)
         # TODO: Fix shapes
         # print(x.size())
-        x = self.to_cls_token(x[:, -1, :])
+        tgt = self.to_cls_token(tgt[:, -1, :])
         # x = x.flatten()
-        x = self.nn1(x)
+        x = self.nn1(tgt)
         # print("After linear layer: ")
         # print(x.size())
         return x
 
-BATCH_SIZE_TRAIN = 32
-BATCH_SIZE_TEST = 32
-N_EPOCHS = 10
-N_TOKENS = 8
+BATCH_SIZE_TRAIN = 64
+BATCH_SIZE_TEST = 64
+N_TOKENS = 1
 LR = 0.0006
 DL_PATH = "/data/bf996/omniglot_merge/" # Use your own path
 SUBSET_SIZE = 100
+MODEL_DIM = 512
 transform = torchvision.transforms.Compose(
      [
      torchvision.transforms.Grayscale(num_output_channels=3),
@@ -209,6 +165,7 @@ idx = [i for i in range(len(omniglot)) if omniglot.imgs[i][1] < SUBSET_SIZE]
 subset = torch.utils.data.Subset(omniglot, idx)
 DATASET = subset
 labels = torch.unique(torch.tensor(omniglot.targets))
+NUM_DATASET_CLASSES = len(labels)
 if DATASET == subset:
     labels = torch.tensor([i for i in range(SUBSET_SIZE)])
 NUM_CLASSES = len(labels)+1 #add 1 for CLS token
@@ -222,6 +179,8 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE_
 
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE_TEST,
                                          shuffle=False)
+
+N_EPOCHS = 10 + (NUM_DATASET_CLASSES // NUM_CLASSES) #Need more epochs for smaller subsets
 
 def train(model, optimizer, data_loader, loss_history, scheduler=None):
     total_samples = len(data_loader.dataset)
@@ -285,12 +244,12 @@ def evaluate(model, data_loader, loss_history):
 # conv_model = torch.nn.Sequential(*list(conv_model.children())).append(new_out)
 
 conv_model = timm.create_model('resnet50', pretrained=True)
-conv_model.fc = torch.nn.Linear(2048, 64)
+conv_model.fc = torch.nn.Linear(2048, MODEL_DIM)
 
 conv_model_direct = timm.create_model('resnet50', pretrained=True)
 conv_model_direct.fc = torch.nn.Linear(2048, NUM_CLASSES)
 
-model = seqTrans(conv_model=conv_model, num_classes=NUM_CLASSES, num_tokens=N_TOKENS).cuda()
+model = seqTrans(conv_model=conv_model, dim=MODEL_DIM, num_classes=NUM_CLASSES, num_tokens=N_TOKENS).cuda()
 # model = conv_model_direct.cuda()
 
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
